05ab0353654d9f86aacc51eeedce2ed775737b2e
MiguelGSilvestre <54369110+miguelsrrobo@users.noreply.github.com>
2024-11-19 21:54:53 -0300

    Add files via upload

---
+++ sim/work/mux2x1_p/_primary.vhd
@@ -0,0 +1,15 @@
+library verilog;
+use verilog.vl_types.all;
+entity mux2x1_p is
+    generic(
+        N               : integer := 4
+    );
+    port(
+        a               : in     vl_logic_vector;
+        b               : in     vl_logic_vector;
+        sel             : in     vl_logic;
+        op              : out    vl_logic_vector
+    );
+    attribute mti_svvh_generic_type : integer;
+    attribute mti_svvh_generic_type of N : constant is 1;
+end mux2x1_p;
